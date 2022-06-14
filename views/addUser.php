44d59e3d76ceec3ac9256bda6feee2f22dbb9d8b
soumia elyakni <selyakni@gmail.com>
2022-06-14 14:29:24 +0100

    addnouvauxjuristefunc

--- views/addUser.php
+++ views/addUser.php
@@ -1,8 +1,4 @@
 <?php
-if(isset($_POST['submit'])){
-$newUser = new UserControllers();
-$newUser-> addUser();
-
-header('location: Tableau' );
-}
+  $newUser = new UserControllers();
+  $newUser-> addUser();
 ?>
--- views/nouveauxJuristes.php
+++ views/nouveauxJuristes.php
@@ -7,11 +7,14 @@ $users =$data-> getAllPUsers();
 
 <h2 class="text-info text-center mb-5" style="margin-top :100px;">Les nouveaux demandes d'inscription Juriste</h2>
 <?php foreach($users as $user): ?>
-<div class="card w-75 mx-auto" style="direction: ltr;">
+<div class="card w-75 mx-auto" style="direction: rtl;">
   <div class="card-body">
     <h5 class="card-title"><?= $user['nom']?>  <?= $user['prenom']?></h5>
     <h6><?= $user['role']?></h6>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <div>
+    <img src="./uploads/<?= $user['cinPhoto']?>" alt="cin" class="w-50" >
+    <img src="./uploads/<?= $user['cipPhoto']?>" alt="cip" class="w-50">
+    </div>
     <a href="#" class="btn btn-success float-end">Accepter</a>
     <a href="#" class="btn btn-warning text-light me-2 float-end">Refuser</a>
   </div>
