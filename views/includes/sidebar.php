577befb41cbb6178f6ad644ad61ad7702270f666
soumia elyakni <selyakni@gmail.com>
2022-06-15 00:06:36 +0100

    side bar link's correction

--- views/includes/sidebar.php
+++ views/includes/sidebar.php
@@ -20,13 +20,13 @@
         </ul>
         <ul class="linklist d-flex flex-column justify-content-center align-items-center  p-1">
             <li class="link mt-3 ps-3 rounded-3 ">
-                <a href="cruduser.html">
+                <a href="Tableau">
                     <img class="mx-auto" src="images/svg/home.svg" alt="">
                     <small class="text-light">لائحة المستخدمين</small>
                 </a>
             </li>
             <li class="link mt-3 ps-3 rounded-3 ">
-                <a href="nouveauxJuristes.php">
+                <a href="nouveauxJuristes">
                     <img class="mx-auto" src="images/svg/cours.svg" alt="">
                     <small class="text-light">طلبات التسجيل</small>
                 </a>
