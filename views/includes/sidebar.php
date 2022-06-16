244545cf64d85c87f1b820a66f42a1eefe708b62
soumia elyakni <selyakni@gmail.com>
2022-06-16 22:21:43 +0100

    conditions side bar

--- views/includes/sidebar.php
+++ views/includes/sidebar.php
@@ -11,10 +11,10 @@
                     <i class="fas fa-times"></i> 
                 </span>
             </li>
+        <?php if (!empty($_SESSION)){ ?>
             <li class="text-center">
                 <img class="rounded-circle w-50 " src="">
-                <h3 class="text-light">Selya</h3>
-                    <!-- <?php // echo $_SESSION['name']?> -->
+                <h3 class="text-light"><?php  echo $_SESSION['User_name']?></h3>
                 <p class="text-info ">Admin</p>
             </li>
         </ul>
@@ -39,4 +39,62 @@
                 </a>
             </li>
         </ul>
+        <?php } else if ($_SESSION['role'] == "عميل"){ ?>
+            <li class="text-center">
+                <img class="rounded-circle w-50 " src="">
+                <h3 class="text-light"><?=  $_SESSION['nom'] ?><?= $_SESSION['prenom'] ?> ?></h3>
+                <p class="text-info ">عميل</p>
+            </li>
+        </ul>
+        <ul class="linklist d-flex flex-column justify-content-center align-items-center  p-1">
+            <li class="link mt-3 ps-3 rounded-3 ">
+                <a href="juristeListe">
+                    <img class="mx-auto" src="images/svg/home.svg" alt="">
+                    <small class="text-light">لائحة المحامين والموثقين</small>
+                </a>
+            </li>
+            <li class="link mt-3 ps-3 rounded-3 ">
+                <a href="mesDemandes">
+                    <img class="mx-auto" src="images/svg/cours.svg" alt="">
+                    <small class="text-light">تفاصيل الطلبات</small>
+                </a>
+            </li>
+
+            <li class="link mt-5 ps-3 rounded-3 ">
+                <a href="logouat">
+                    <small class="text-light">تسجيل الخروج</small>
+                    <img class="ms-1" src="images/svg/logout.svg" alt="">
+                </a>
+            </li>
+        </ul>
+        <?php } else { ?>
+            <li class="text-center">
+                <img class="rounded-circle w-50 " src="">
+                <h3 class="text-light"><?= $_SESSION['nom']?><?= $_SESSION['prenom']?></h3>
+                <p class="text-info "><?= $_SESSION['role']?></p>
+            </li>
+        </ul>
+        <ul class="linklist d-flex flex-column justify-content-center align-items-center  p-1">
+            <li class="link mt-3 ps-3 rounded-3 ">
+                <a href="Tableau">
+                    <img class="mx-auto" src="images/svg/home.svg" alt="">
+                    <small class="text-light">لائحة الطلبات</small>
+                </a>
+            </li>
+            <li class="link mt-3 ps-3 rounded-3 ">
+                <a href="nouveauxJuristes">
+                    <img class="mx-auto" src="images/svg/cours.svg" alt="">
+                    <small class="text-light">لائحة المواعيد</small>
+                </a>
+            </li>
+
+            <li class="link mt-5 ps-3 rounded-3 ">
+                <a href="logouat">
+                    <small class="text-light">تسجيل الخروج</small>
+                    <img class="ms-1" src="images/svg/logout.svg" alt="">
+                </a>
+            </li>
+        </ul>
+        <?php } ?>
+
     </nav>
