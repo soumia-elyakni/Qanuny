de6eab91bfea0b63a4ef99939b94a910f0cea817
soumia elyakni <selyakni@gmail.com>
2022-06-16 22:22:37 +0100

    loginUser

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     $home = new HomeController();
 
-    $pages = ['home','loginAdmin', 'Tableau','nouveauxJuristes','juristeListe','demandes','rendezVous', 'addUser','deleteAddJuriste','deleteUser', 'confirmUser', 'logouat'];
+    $pages = ['home','loginAdmin', 'Tableau','nouveauxJuristes','juristeListe','demandes','rendezVous', 'addUser','deleteAddJuriste','deleteUser', 'confirmUser','loginUser', 'logouat'];
 
     if(isset($_GET['page'])){
         if(in_array($_GET['page'],$pages)){
