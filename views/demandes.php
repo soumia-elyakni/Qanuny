93bbfd5519f9f2d66859b916f11225de17a4f863
soumia elyakni <selyakni@gmail.com>
2022-06-16 23:36:58 +0100

    front end users

---
+++ views/demandes.php
@@ -0,0 +1,48 @@
+<?php
+if (isset($_SESSION['login'])){
+    if($_SESSION['role']== "عميل"){
+        Redirect::to('juristeListe');
+    } 
+    } else {
+        Redirect::to('home');
+    }
+
+
+include_once 'includes/sidebar.php';
+?>
+
+<main style="position: absolute; z-index: 20; width: calc(100% - 270px); top: 62px; right : 270px; padding: 20px;">
+    <section class=" d-flex align-items-center px-3 justify-content-between" style=" margin-top : 40px; direction:rtl;">
+        <h2 class="text-info text-center mb-5" style="margin-top :100px;">لائحة الطلبات</h2>
+   
+    <select name="statut" class="select mt-3 float-start" id="statut" style="width : 190px">
+             <option selected="selected">الكل</option>
+             <option value="عميل">الانتظار</option>
+             <option value="محام">مقبول</option>
+    </select>
+    </section>
+
+<?php ?>
+<div class="card m-auto p-4 mb-4">
+  <div class="card-header">
+    <h6>علي العال</h6>
+    <p>04/06/2022</p>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">اجراءات الحصول على الحضانة</h5>
+    <p class="card-text">لوريم ابسوم بننبتنتبلمنتلقهثتلقثهتلقهثتلقهتلقهخلاقهخلاقخلتتلتل</p>
+    <p class="card-text">لوريم ابسوم بننبتنتبلمنتلقهثتلقثهتلقهثتلقهتلقهخلاقهخلاقخلتتلتل</p>
+<div class=" d-flex float-start">  
+<form class="ms-1">
+    <a href="#" class="btn btn-primary">القبول</a>
+</form>
+<form class="me-1">
+    <a href="#" class="btn btn-primary">الرفض</a>
+</form>
+</div> 
+  </div>
+</div>
+<?php ?>
+
+
+</main>
--- views/juristeListe.php
+++ views/juristeListe.php
@@ -1,1 +1,27 @@
-juriste liste 
+<?php
+
+
+
+include_once 'includes/sidebar.php';
+?>
+<main style="position: absolute; z-index: 20; width: calc(100% - 270px); top: 62px; right : 270px; padding: 20px;">
+<section class=" d-flex align-items-center px-3 justify-content-between" style=" margin-top : 40px; direction:rtl;">
+        <h2 class="text-info text-center mb-5" style="margin-top :100px;">لائحة المحامين والموثقين</h2>
+   
+    <select name="statut" class="select mt-3 float-start" id="statut" style="width : 190px">
+             <option selected="selected">الكل</option>
+             <option value="محام">محام</option>
+             <option value="موثق">موثق</option>
+    </select>
+    </section>
+<div class="card py-3 px-2" style="width: 18rem;">
+  <img src="public/image/mr_nobody_new.svg" class="card-img-top w-50 m-auto" alt="photo">
+  <div class="card-body m-auto">
+    <h5 class="card-title">الاسم الشخصي والعائلي</h5>
+    <p class="card-text">موثق</p>
+    <a href="#" class="btn btn-primary float-start">ارسال طلب</a>
+  </div>
+</div>
+
+
+</main>
