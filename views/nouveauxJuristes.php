2ba9db54f5a7dc74fb040dd0d7354898e3b8fbb0
soumia elyakni <selyakni@gmail.com>
2022-06-17 01:03:35 +0100

    margin

--- views/nouveauxJuristes.php
+++ views/nouveauxJuristes.php
@@ -14,13 +14,14 @@ include_once 'includes/sidebar.php';
 <h2 class="text-info text-center mb-5" style="margin-top :100px;">لائحة الطلبات الجديدة</h2>
 <?php foreach($users as $user): ?>
 <div class="card w-75 mx-auto my-2" style="direction: rtl;">
-  <div class="card-body">
+  <div class="card-body pb-5">
     <h5 class="card-title"><?= $user['nom']?>  <?= $user['prenom']?></h5>
     <h6><?= $user['role']?></h6>
     <div class="row d-flex p-2">
     <img src="./uploads/<?= $user['cinPhoto']?>" alt="cin" class="col-6 w-50" >
     <img src="./uploads/<?= $user['cipPhoto']?>" alt="cip" class="col-6 w-50">
     </div>
+    <div>
     <form method="POST" action="confirmUser">
       <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
       <button class="btn btn-primary text-light float-end">Accepter</button>
@@ -29,7 +30,7 @@ include_once 'includes/sidebar.php';
       <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
       <button class="btn btn-secondary text-light me-2 float-end">Refuser</button>
     </form>
-
+    </div>
     
   </div>
 </div>
