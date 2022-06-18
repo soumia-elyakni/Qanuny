13be491c7d54088218bea2929f6a231b1792cc80
soumia elyakni <selyakni@gmail.com>
2022-06-18 17:50:02 +0100

    js to header location

--- app/classe/Redirect.php
+++ app/classe/Redirect.php
@@ -1,10 +1,7 @@
 <?php
-
 class Redirect{
-
     static public function to($page) {
-        echo "<script type='text/javascript'>document.location.replace('$page');</script>";
+        header("location: $page");
+        die();
 }
 }
-
-?>
