0aae3c8382e965c486f8f0acf31cca878b9d31dc
soumia elyakni <selyakni@gmail.com>
2022-06-21 09:03:22 +0100

    delete juriste

--- views/deleteUser.php
+++ views/deleteUser.php
@@ -2,17 +2,13 @@
 if(isset($_POST['id'])){
     $exitUser = new UserControllers();
     $exitUser -> deleteUser();
-    // Redirect::to('Tableau');
+    Redirect::to('Tableau');
+}
+
+if(isset($_POST['jurId'])){
+  $data['id']= $_POST['jurId'];
+  $exitUser = new JuristeControllers();
+  $exitUser -> deleteJuriste();
+  Redirect::to('TableauJuriste');
 }
 ?>
-
-<div class="container-fluid border border-secondary position-absolute top-50 mx-auto  translate-middle w-50 " >
-  <div class="card-header bg-transparent border-secondary"></div>
-  <div class="card-body text-primary p-4">
-    
-    <h4 class="card-text">لقد تم حذف الحساب بنجاح</h4>
-  </div>
-  <div class="card-footer bg-transparent border-secondary">
-  <a href="Tableau" class="btn btn-primary float-start mb-2">لائحة المستخدمين</a>
-  </div>
-</div>
