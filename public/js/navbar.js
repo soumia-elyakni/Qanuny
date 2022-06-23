f9b75f3cb6a1ab623bf099ad706988c61b1ccc32
soumia elyakni <selyakni@gmail.com>
2022-06-23 09:34:48 +0100

    Logo color in scrolling

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,9 +1,13 @@
-
+let title = document.getElementById("title")
 let header = document.querySelector('.navbar');
 window.addEventListener('scroll',function(){
     if(window.scrollY > 30) {
         header.classList.add('scrolling-active')
+        title.style.color = "#0d6efd";
     } else {
-        header.classList.remove('scrolling-active')
+        
+        header.classList.remove('scrolling-active');
+        title.style.color = "aliceblue";
+       
     }
 })
